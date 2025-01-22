ee6ae6cd9be42432508e28bd153aee7236e70136
Troy200 <kuriyanthomas27@gmail.com>
2025-01-22 17:14:45 +0100

    Add files via upload

---
+++ Les 3 HW.py
@@ -0,0 +1,22 @@
+import pgzrun 
+
+WIDTH= 400
+HEIGHT=400
+
+def draw():
+    screen.fill("red")
+    screen.draw.line((100,100),(100,300), "white")
+    screen.draw.line((100,100),(300,200), "white")
+    screen.draw.line((100,300),(300,200), "white")
+
+
+
+
+
+
+
+
+
+
+
+pgzrun.go()
